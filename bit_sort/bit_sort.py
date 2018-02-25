9b8139676b547b919fdf01ff3fc0a552385207da
Michael <panmihaurkin@icloud.com>
2018-02-25 17:10:03 +0700

    Added basic sorting function

--- bit_sort/bit_sort.py
+++ bit_sort/bit_sort.py
@@ -7,6 +7,27 @@ def bubble_sort(ar):
                 ar[i + 1] = temp
     return ar
 
+def bin_search(ar, n):
+    bubble_sort(ar)
+    low = 0
+    high = len(ar) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+
+        if n == ar[mid]:
+            print("value", n, "is located at index of", mid)
+            return 0
+        elif n > ar[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return 1
+
+
+
+
+
 
 
             
